4413e81de78538f0e452ad7b466c67e94870d920
Umang Kumar <88194464+umangkumarchaudhary@users.noreply.github.com>
2024-10-09 19:34:32 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,2 @@
-// service-worker.js
 
-self.addEventListener('push', event => {
-    const data = event.data.json();
-    console.log('Push received:', data);
-    
-    const options = {
-      body: data.body,
-      icon: '/path-to-your-icon.png', // Optional: path to an icon
-      badge: '/path-to-your-badge.png' // Optional: path to a badge
-    };
-  
-    event.waitUntil(
-      self.registration.showNotification(data.title, options)
-    );
-  });
   
